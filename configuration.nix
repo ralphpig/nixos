1be341f2be38c011a3a488b7cb0d136663a3995d
Mike <mail@ralphpig.dev>
2024-12-10 00:00:42 -0500

    feat(wip): plymouth

--- configuration.nix
+++ configuration.nix
@@ -28,6 +28,31 @@
         useOSProber = true;
       };
     };
+
+    initrd.systemd.enable = true;
+    plymouth = {
+      enable = true;
+      theme = "cross_hud";
+      themePackages = with pkgs; [
+        # By default we would install all themes
+        (adi1090x-plymouth-themes.override {
+          selected_themes = [ "cross_hud" ];
+        })
+      ];
+    };
+
+    # Enable "Silent Boot"
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
   };
 
   networking = {
