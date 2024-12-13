862721a5db63bceebc8d9c2b24bd1ef9c68fa5f2
Mike <mail@ralphpig.dev>
2024-12-12 23:00:15 -0500

    nix: fix suspend resume

--- gpu.nix
+++ gpu.nix
@@ -21,7 +21,9 @@
     # Enable this if you have graphical corruption issues or application crashes after waking
     # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
     # of just the bare essentials.
-    powerManagement.enable = false;
+    
+    # Enabled because suspend was not working
+    powerManagement.enable = true;
 
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
